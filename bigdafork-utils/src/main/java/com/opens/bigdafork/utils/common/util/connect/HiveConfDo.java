1a402c8c825ca6022392f9a1c8bb159ffa099f49
conghe2402 <haohaohao2004@qq.com>
2020-01-03 15:11:07 +0800

    common commit

--- bigdafork-utils/src/main/java/com/opens/bigdafork/utils/common/util/connect/HiveConfDo.java
+++ bigdafork-utils/src/main/java/com/opens/bigdafork/utils/common/util/connect/HiveConfDo.java
@@ -1,6 +1,8 @@
 package com.opens.bigdafork.utils.common.util.connect;
 
+import com.opens.bigdafork.utils.common.config.EnvConfigsLoader;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +15,9 @@ public class HiveConfDo extends ConfDo {
     @Override
     public Configuration prepareConf(Configuration configuration) {
         LOGGER.debug("Hive conf initialize");
+        EnvConfigsLoader env = EnvConfigsLoader.getInstance();
+        LOGGER.debug("load core-site file : " + env.getCoreSiteXml());
+        configuration.addResource(new Path(env.getCoreSiteXml()));
         return configuration;
     }
 }
