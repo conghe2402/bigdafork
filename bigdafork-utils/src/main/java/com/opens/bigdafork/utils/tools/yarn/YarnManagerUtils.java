f9815865983c6309044d48561f5d039666da1eaf
conghe2402 <haohaohao2004@qq.com>
2020-06-24 13:59:26 +0800

    common commit

--- bigdafork-utils/src/main/java/com/opens/bigdafork/utils/tools/yarn/YarnManagerUtils.java
+++ bigdafork-utils/src/main/java/com/opens/bigdafork/utils/tools/yarn/YarnManagerUtils.java
@@ -8,7 +8,6 @@ import org.apache.hadoop.yarn.api.records.YarnClusterMetrics;
 import org.apache.hadoop.yarn.client.api.YarnClient;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
-import org.apache.hadoop.yarn.proto.YarnProtos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
